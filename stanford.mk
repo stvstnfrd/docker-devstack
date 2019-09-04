39bba369f0134a28f26ef7686464e2f46c8b9e5d
stv <stv@stanford.edu>
2019-09-04 16:21:17 -0700

    Add Makefile helpers to run tests in devstack

--- stanford.mk
+++ stanford.mk
@@ -15,3 +15,18 @@ from_scratch:
 	make dev.provision
 	make dev.up
 	make logs
+
+test:
+	docker exec -t edx.devstack.lms bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform/ && ./scripts/circle-ci-tests.sh'
+
+test-quality:
+	docker exec -t edx.devstack.lms bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform/ && CIRCLE_NODE_TOTAL=4 CIRCLE_NODE_INDEX=0 ./scripts/circle-ci-tests.sh'
+
+test-lms:
+	docker exec -t edx.devstack.lms bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform/ && CIRCLE_NODE_TOTAL=4 CIRCLE_NODE_INDEX=1 ./scripts/circle-ci-tests.sh'
+
+test-cms:
+	docker exec -t edx.devstack.lms bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform/ && CIRCLE_NODE_TOTAL=4 CIRCLE_NODE_INDEX=2 ./scripts/circle-ci-tests.sh'
+
+test-common:
+	docker exec -t edx.devstack.lms bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform/ && CIRCLE_NODE_TOTAL=4 CIRCLE_NODE_INDEX=3 ./scripts/circle-ci-tests.sh'
